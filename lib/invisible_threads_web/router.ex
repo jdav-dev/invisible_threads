ba0a39a71c558d60d767f9d8869e478130393852
Josh Davis <josh@jdav.dev>
2025-06-02 00:03:30 +0000

    Run `mix phx.gen.auth Accounts User users`

--- lib/invisible_threads_web/router.ex
+++ lib/invisible_threads_web/router.ex
@@ -1,6 +1,8 @@
 defmodule InvisibleThreadsWeb.Router do
   use InvisibleThreadsWeb, :router
 
+  import InvisibleThreadsWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule InvisibleThreadsWeb.Router do
     plug :put_root_layout, html: {InvisibleThreadsWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule InvisibleThreadsWeb.Router do
       live_dashboard "/dashboard", metrics: InvisibleThreadsWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", InvisibleThreadsWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{InvisibleThreadsWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", InvisibleThreadsWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{InvisibleThreadsWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
